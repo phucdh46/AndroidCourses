649a80c416d1a9a3b61f855a4877225e7687311f
DHP <june041996@gmail.com>
2022-10-17 10:51:53 +0700

    add menu

--- Android fundamentals/Android fundamentals 04.1 Clickable images/app/src/main/java/com/example/clickableimages/MainActivity.kt
+++ Android fundamentals/Android fundamentals 04.1 Clickable images/app/src/main/java/com/example/clickableimages/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.clickableimages
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +20,9 @@ class MainAcivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener() {
             val intent = Intent(this, OrderActivity::class.java)
@@ -26,18 +31,34 @@ class MainAcivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> displayToast(getString(R.string.action_order_message))
+            R.id.action_contact -> displayToast(getString(R.string.action_contact_message))
+            R.id.action_favorites -> displayToast(getString(R.string.action_favorites_message))
+            R.id.action_status -> displayToast(getString(R.string.action_status_message))
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     fun showDonutOrder(view: View) {
-        mOrderMessage = getString(R.string.donut_order_message);
+        mOrderMessage = getString(R.string.donut_order_message)
         displayToast(mOrderMessage!!)
     }
 
     fun showIceCreamOrder(view: View) {
-        mOrderMessage = getString(R.string.ice_cream_message);
+        mOrderMessage = getString(R.string.ice_cream_message)
         displayToast(mOrderMessage!!)
     }
 
     fun showFroyoOrder(view: View) {
-        mOrderMessage = getString(R.string.froyo_message);
+        mOrderMessage = getString(R.string.froyo_message)
         displayToast(mOrderMessage!!)
     }
 
