81468ad0f065b644dbd76eaff8f62c1f2a0ae699
DHP <june041996@gmail.com>
2022-10-27 17:37:21 +0700

    RxAndroid

---
+++ RxAndroid/CountriesFinder/app/src/main/java/com/example/countriesfinder/CountryApplication.kt
@@ -0,0 +1,4 @@
+package com.example.countriesfinder
+
+class CountryApplication {
+}
---
+++ RxAndroid/RxAndroidSample/app/src/main/java/com/example/rxandroidsample/RxLifecycleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rxandroidsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RxLifecycleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_lifecycle)
+    }
+}
